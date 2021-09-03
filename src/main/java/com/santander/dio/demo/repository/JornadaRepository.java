fb515df8fd0e69da684198351398cd1da5d2f03e
josepjunior <josepsjunior.df@gmail.com>
2021-09-03 01:12:31 -0300

    Commit do projeto finalizado

---
+++ src/main/java/com/santander/dio/demo/repository/JornadaRepository.java
@@ -0,0 +1,9 @@
+package com.santander.dio.demo.repository;
+
+import com.santander.dio.demo.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho, Long> {
+}
